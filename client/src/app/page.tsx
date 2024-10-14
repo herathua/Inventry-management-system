1b4a44b194ac73a747cb91dc98b442d25d644f61
herathua <110981456+herathua@users.noreply.github.com>
2024-10-14 23:09:17 +0530

    initialize Next.js project with TypeScript, Tailwind CSS, and basic configuration files

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
+
+export default function Home() {
+  return (
+    <main>
+      hello world
+    </main>  
+  );
+}
